c174f1ce73a5e611e643ad4443017b4afc705d6d
franck <franck@diabeloop.fr>
2020-10-19 12:04:45 +0200

    leverage artifact_go

---
+++ artifact.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -ex
+
+if [ -f artifact_go.sh ]; then
+  rm -f artifact_go.sh
+fi
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add-platform-project/artifact/artifact_go.sh'
+chmod +x artifact_go.sh
+
+export ARTIFACT_GO_VERSION='1.11.4'
+
+# make sure build.sh can be executed 
+chmod 755 build.sh
+# Disable binary deployment in artifact_go.sh 
+export ARTIFACT_DEPLOY=false
+export ARTIFACT_BUILD=true
+export BUILD_OPENAPI_DOC=false
+export SECURITY_SCAN=true
+./artifact_go.sh data 
