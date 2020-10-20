520e1f13cf3361e9ac6d91ee589f6dfc96f2bea6
franck <franck@diabeloop.fr>
2020-10-20 13:59:59 +0200

    apply review changes

--- artifact.sh
+++ artifact.sh
@@ -5,13 +5,13 @@ if [ -f artifact_go.sh ]; then
 fi
 wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add-platform-project/artifact/artifact_go.sh'
 chmod +x artifact_go.sh
+chmod +x build.sh
 
 export ARTIFACT_GO_VERSION='1.11.4'
-
-chmod 755 build.sh
 # Disable binary deployment in artifact_go.sh as it is done by the Make command
 export ARTIFACT_DEPLOY=false
 export ARTIFACT_BUILD=true
 export BUILD_OPENAPI_DOC=false
 export SECURITY_SCAN=true
-./artifact_go.sh data 
+
+./artifact_go.sh service=data 
