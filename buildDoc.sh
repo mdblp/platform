a932706dff46d957c750d91503b909ea55652f19
Cedric Perotto <cedric.perotto@diabeloop.fr>
2022-02-23 14:38:32 +0100

    Fix buildDoc

--- buildDoc.sh
+++ buildDoc.sh
@@ -7,7 +7,7 @@ export GO111MODULE="on"
 
 if [ ! -x "$GOPATH/bin/swag" ]; then
   echo "Getting swag..."
-  go install github.com/swaggo/swag/cmd/swag@latest
+  go install github.com/swaggo/swag/cmd/swag
 fi
 
 $GOPATH/bin/swag --version
